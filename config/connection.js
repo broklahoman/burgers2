abbb891ab4b0052bf9298baddb8ffe6bdf2599fb
Jonathan Harris <j.harris.onathan@gmail.com>
2017-05-06 23:17:18 -0500

    new project

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else { 
+  connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burger_db"
+  });
+};
+
+connection.connect();
+
+// Export connection for our ORM to use.
+module.exports = connection;
